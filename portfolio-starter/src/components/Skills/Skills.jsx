182a49ae07683e1ac5a65374ba09949811501be9
Aditya Raj <aadiyadav56837492@gmail.com>
2022-07-31 10:45:11 +0530

    skill

--- portfolio-starter/src/components/Skills/Skills.jsx
+++ portfolio-starter/src/components/Skills/Skills.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import "./Skills.css"
 import Skillbox from '../skillbox/Skillbox';
+import { AiFillGithub } from 'react-icons/ai';
 import { ImHtmlFive} from 'react-icons/im';
+import { FaCss3Alt,FaReact,FaNodeJs} from 'react-icons/fa';
+import { SiJavascript,SiRedux,SiChakraui,SiNpm} from 'react-icons/si';
 const Skills = () => {
   return (
     <div className="skills">
@@ -19,8 +22,20 @@ const Skills = () => {
      </div>
     {/* right */}
     <div className="s-right">
-      <Skillbox logo={<ImHtmlFive />}/>
-   <h1>HTML</h1>
+      <div>
+         <Skillbox logo={<ImHtmlFive size={60} />} name={"HTML"}/>
+         <Skillbox logo={<FaCss3Alt size={60}/>} name={"CSS"}/>
+         <Skillbox logo={<SiJavascript size={60}/>} name={"JS"}/>
+         <Skillbox logo={<FaReact size={60}/>} name={"React"}/>
+         <Skillbox logo={<SiRedux size={60}/>} name={"Redux"}/>
+      </div>
+       <div>
+         <Skillbox logo={<AiFillGithub size={60} />} name={"Git"}/>
+         <Skillbox logo={<SiChakraui size={60}/>} name={"Chakra"}/>
+         <Skillbox logo={<FaNodeJs size={60}/>} name={"Node"}/>
+         <Skillbox logo={<SiNpm size={60}/>} name={"NPM"}/>
+         
+      </div>
     </div>
     </div>
   )
--- portfolio-starter/src/components/skillbox/Skillbox.jsx
+++ portfolio-starter/src/components/skillbox/Skillbox.jsx
@@ -4,10 +4,9 @@ import "./Skillbox.css"
 const Skillbox = ({logo,name}) => {
   return (
     <div className="skillbox">
-       <h1>{logo}</h1>
-       <h1>{name}</h1>
-  
- </div>
+       <span>{logo}</span>
+       <span>{name}</span>
+   </div>
   )
 }
 
